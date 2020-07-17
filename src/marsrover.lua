ed7c271368828172e83ed3952c1b3d81b06c69d6
romo <robmoore121@gmail.com>
2020-07-17 11:37:21 +0800

    commit

--- src/marsrover.lua
+++ src/marsrover.lua
@@ -27,11 +27,54 @@ function nextInstruction(simulatedRover)
     return string.sub(simulatedRover.remainingInstructions, 0, 1)
 end
 
+function turnLeft(simulatedRover)
+    local leftTurns = {
+        ["N"] = "W",
+        ["S"] = "E",
+        ["E"] = "N",
+        ["W"] = "S"
+    }
+    simulatedRover.direction = leftTurns[simulatedRover.direction]
+end
+
+function turnRight(simulatedRover)
+    local rightTurns = {
+        ["N"] = "E",
+        ["S"] = "W",
+        ["E"] = "S",
+        ["W"] = "N"
+    }
+    simulatedRover.direction = rightTurns[simulatedRover.direction]
+end
+
+function movementComponents(simulatedRover)
+    local dxComponents = {
+        ["N"] = 0,
+        ["S"] = 0,
+        ["E"] = 1,
+        ["W"] = -1
+    }
+    local dyComponents = {
+        ["N"] = 1,
+        ["S"] = -1,
+        ["E"] = 0,
+        ["W"] = 0
+    }
+    return {
+        dx = dxComponents[simulatedRover.direction],
+        dy = dyComponents[simulatedRover.direction]
+    }
+end
+
 function simulateNextInstruction(simulatedRover)
     if nextInstruction(simulatedRover) == "M" then
-        simulatedRover.y = simulatedRover.y + 1
+        local components = movementComponents(simulatedRover)
+        simulatedRover.y = simulatedRover.y + components.dy
+        simulatedRover.x = simulatedRover.x + components.dx
     elseif nextInstruction(simulatedRover) == "L" then
-        simulatedRover.direction = "W"
+        turnLeft(simulatedRover)
+    elseif nextInstruction(simulatedRover) == "R" then
+        turnRight(simulatedRover)
     end
     simulatedRover.remainingInstructions = beheadString(simulatedRover.remainingInstructions)
 end
--- test/marsrovertest.lua
+++ test/marsrovertest.lua
@@ -5,8 +5,8 @@ require('src/marsrover')
 --noinspection UnassignedVariableAccess
 tablex = require('pl.tablex')
 
-function testMovingNorth()
-    local simulatedRovers = simulate(plateau(5, 5), simulatedRover(1, 2, "N", "M"))
+function testFirstExampleRover()
+    local simulatedRovers = simulate(plateau(5, 5), simulatedRover(1, 2, "N", "LMLMLMLMM"))
     luaunit.assertEquals(tablex.size(simulatedRovers), 1)
     local firstRover = simulatedRovers[1]
     luaunit.assertEquals(firstRover.x, 1)
@@ -15,14 +15,70 @@ function testMovingNorth()
     luaunit.assertEquals(firstRover.remainingInstructions, "")
 end
 
-function testTurning()
-    local simulatedRovers = simulate(plateau(5, 5), simulatedRover(1, 2, "N", "ML"))
+function testSecondExampleRover()
+    local simulatedRovers = simulate(plateau(5, 5), simulatedRover(3, 3, "E", "MMRMMRMRRM"))
     luaunit.assertEquals(tablex.size(simulatedRovers), 1)
     local firstRover = simulatedRovers[1]
-    luaunit.assertEquals(firstRover.x, 1)
-    luaunit.assertEquals(firstRover.y, 3)
-    luaunit.assertEquals(firstRover.direction, "W")
+    luaunit.assertEquals(firstRover.x, 5)
+    luaunit.assertEquals(firstRover.y, 1)
+    luaunit.assertEquals(firstRover.direction, "E")
     luaunit.assertEquals(firstRover.remainingInstructions, "")
+end
+
+function testTurningLeft()
+    local rover = simulatedRover(1, 2, "N", "LLLL")
+    simulateNextInstruction(rover)
+    luaunit.assertEquals(rover.direction, "W")
+    simulateNextInstruction(rover)
+    luaunit.assertEquals(rover.direction, "S")
+    simulateNextInstruction(rover)
+    luaunit.assertEquals(rover.direction, "E")
+    simulateNextInstruction(rover)
+    luaunit.assertEquals(rover.direction, "N")
+end
+
+function testTurningRight()
+    local rover = simulatedRover(1, 2, "N", "RRRR")
+    simulateNextInstruction(rover)
+    luaunit.assertEquals(rover.direction, "E")
+    simulateNextInstruction(rover)
+    luaunit.assertEquals(rover.direction, "S")
+    simulateNextInstruction(rover)
+    luaunit.assertEquals(rover.direction, "W")
+    simulateNextInstruction(rover)
+    luaunit.assertEquals(rover.direction, "N")
+end
+
+function testMovingNorth()
+    local rover = simulatedRover(1, 2, "N", "M")
+    simulateNextInstruction(rover)
+    luaunit.assertEquals(rover.x, 1)
+    luaunit.assertEquals(rover.y, 3)
+    luaunit.assertEquals(rover.direction, "N")
+end
+
+function testMovingSouth()
+    local rover = simulatedRover(1, 3, "S", "M")
+    simulateNextInstruction(rover)
+    luaunit.assertEquals(rover.x, 1)
+    luaunit.assertEquals(rover.y, 2)
+    luaunit.assertEquals(rover.direction, "S")
+end
+
+function testMovingWest()
+    local rover = simulatedRover(5, 3, "W", "M")
+    simulateNextInstruction(rover)
+    luaunit.assertEquals(rover.x, 4)
+    luaunit.assertEquals(rover.y, 3)
+    luaunit.assertEquals(rover.direction, "W")
+end
+
+function testMovingEast()
+    local rover = simulatedRover(5, 3, "E", "M")
+    simulateNextInstruction(rover)
+    luaunit.assertEquals(rover.x, 6)
+    luaunit.assertEquals(rover.y, 3)
+    luaunit.assertEquals(rover.direction, "E")
 end
 
 os.exit(luaunit.LuaUnit.run())
